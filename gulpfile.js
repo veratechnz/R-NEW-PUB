08d346bca66ae84654dea03c66d35edac27c2347
unknown <user@REINZLPT1402.reinz.local>
2015-09-25 10:00:56 +1200

    250915

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,39 @@
+//Gulp Variables
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+	sass = require('gulp-sass');
+ 	livereload = require('gulp-livereload');
+ 	connect = require('gulp-connect');
+ 
+//Server Task
+gulp.task('serve', function(event) {
+    connect.server({
+        root: '',
+        port: 1988,
+        livereload: true
+    });
+});
 
-//Styles task
+//Styles Task
 gulp.task('styles', function() {
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'))
+        .pipe(connect.reload());
+});
+
+//HTML Task 
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+
+//Watch Task
+gulp.task('watch', function(){
+	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('./*.html', ['html']);
+
 });
 
 //Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
-});
+gulp.task('default', ['serve', 'styles', 'html', 'watch']);
+
