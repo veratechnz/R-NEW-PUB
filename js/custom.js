68d06f8b82f613b05ed24cb071d62b1bbcc8b356
veratechnz <jared@veratech.co.nz>
2015-10-16 11:56:09 +1300

    latest branch

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ $(function() {
     function init(){
         superFishNav();
         parallaxGo();
-        subNavMember();
+        // subNavMember();
         hoverSubNav();
      }
 
@@ -34,91 +34,67 @@ $(function() {
     }
 
 
-
-
-
-    //Testing for toggle of subnav
-    function boxShadowCheck(){
-        // if else for removing the box shadow with speed
-        ($('#memberSubNav').hasClass('no-shadow')) ? 
-            $('#memberSubNav').toggleClass('no-shadow').promise()
-            .done(function(){
-            $('#memberSubNavB').slideToggle('linear');
-          })
-         : 
-          $('#memberSubNavB').slideToggle('linear', function(){
-           $('#memberSubNav').toggleClass('no-shadow');
-          });       
-        // if else ends
-    }
-
-    function subNavMember() {
-      $('#mainLogo').click(function(event){
-        event.stopPropagation();
-        event.preventDefault();
-        boxShadowCheck();
-      });
-    } 
-
-
-
-
-
-
-//Member Sub Nav Test
-
+//Member Sub Interaction
   function openSub(){
         // if else for removing the box shadow with speed
-
           $('#memberSubNavB').slideDown('linear');
           $('#memberSubNav').addClass('no-shadow');
+          return;
        
         // if else ends
   }
 
-
   function closeSub(){
             $('#memberSubNavB').slideUp('linear');
             $('#memberSubNav').removeClass('no-shadow');
+            return;
   }
 
-    function hoverSubNav(){
-      //Open the subNav
-      $('#dataSub').mouseenter(function(){
-        openSub(); 
-      });
+  function hoverSubNav(){
+    //Open the subNav
+    $('#membershipSub').mouseenter(function(){
+      console.log('1');
+      openSub(); 
       unHoverSubNav();
-    }
-
+    }); 
+  }
 
   function unHoverSubNav(){
-      $('#dataSub').mouseleave(function(){
-        if($('#memberSubNavB').is(':hover')) {
+      $('#membershipSub').mouseleave(function(){
+          //Tiny setTimeout milisecond interval to combat IE and Firefox bug.
+        setTimeout(function() {         
+          if ($('#memberSubNavB:hover').length > 0) {
               isMemberSubNavB();
-            }
-            else {
+            } else if ($('#membershipSub:hover').length > 0){
+              console.log('4');
+              return;
+            } else {
               setTimeout(function(){
-                closeSub();
-              }, 100);
-            }
+                console.log('2');
+                closeSub();  
+              }, 300);  
+            }}, 
+            //Tiny milisecond interval to combat IE and Firefox bug. 
+            500);
       });
 
-            function isMemberSubNavB(){
-            if($('#memberSubNavB').is(':hover') || $('#dataSub').is(':hover')) {
-              return;
-              } else {
-
-                $('#memberSubNavB').mouseleave(function(){
-                  setTimeout(function(){
-                    closeSub();
-                  }, 100);
-                });
-
-            }
-
-            }
-
+            //Function to check once hover state is on MemberSubNavB
+              function isMemberSubNavB(){
+                  $('#memberSubNavB').mouseleave(function(){
+                    if ($('#membershipSub:hover').length > 0) {
+                      return;
+                    } else {
+                    setTimeout(function(){
+                      console.log('3');
+                      closeSub();
+                      return;
+                    }, 300);
+                    }
+                  });
+              }
   }
+
+
 
 
 
