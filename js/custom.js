fbdca07c0a2d55ef6dbddf24e288c7ef2f07a091
veratech <jared@veratech.co.nz>
2015-10-27 08:22:10 +1300

    update

--- js/custom.js
+++ js/custom.js
@@ -34,65 +34,8 @@ $(function() {
     }
 
 
-//Member Sub Interaction
-  function openSub(){
-        // if else for removing the box shadow with speed
-          $('#memberSubNavB').slideDown('linear');
-          $('#memberSubNav').addClass('no-shadow');
-          return;
-       
-        // if else ends
-  }
 
-  function closeSub(){
-            $('#memberSubNavB').slideUp('linear');
-            $('#memberSubNav').removeClass('no-shadow');
-            return;
-  }
 
-  function hoverSubNav(){
-    //Open the subNav
-    $('#membershipSub').mouseenter(function(){
-      console.log('1');
-      openSub(); 
-      unHoverSubNav();
-    }); 
-  }
-
-  function unHoverSubNav(){
-      $('#membershipSub').mouseleave(function(){
-          //Tiny setTimeout milisecond interval to combat IE and Firefox bug.
-        setTimeout(function() {         
-          if ($('#memberSubNavB:hover').length > 0) {
-              isMemberSubNavB();
-            } else if ($('#membershipSub:hover').length > 0){
-              console.log('4');
-              return;
-            } else {
-              setTimeout(function(){
-                console.log('2');
-                closeSub();  
-              }, 300);  
-            }}, 
-            //Tiny milisecond interval to combat IE and Firefox bug. 
-            500);
-      });
-
-            //Function to check once hover state is on MemberSubNavB
-              function isMemberSubNavB(){
-                  $('#memberSubNavB').mouseleave(function(){
-                    if ($('#membershipSub:hover').length > 0) {
-                      return;
-                    } else {
-                    setTimeout(function(){
-                      console.log('3');
-                      closeSub();
-                      return;
-                    }, 300);
-                    }
-                  });
-              }
-  }
 
 
 
