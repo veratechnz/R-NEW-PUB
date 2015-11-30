53917ba89e52bf9bd49c596d6b4043685842b7cf
veratech <jared@veratech.co.nz>
2015-11-30 13:27:26 +1300

    301115

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,35 @@
 // Custom JS File
 $(function() {
 
+	if (document.documentElement.clientWidth < 1050) {
+
+		$(window).ready(function(){
+
+			$(document).one('mousemove', function(e){
+			    menuHelper();
+			});
+
+			$(window).on("touchstart", function(ev) {
+				menuHelper();
+			});
+
+			function menuHelper(){
+				$('#navToggle').tooltip('show');
+					setTimeout(finishTooltip, 6000)
+				$('#navToggle').on('click', function(){
+					finishTooltip();
+				});
+			}
+
+			function finishTooltip(){
+				$('#navToggle').tooltip('hide');
+			}
+
+		});
+	
+	}
 
 
+	// https://responsivedesign.is/develop/javascript/conditionally-load-javascript-based-on-media-query
 
 }); //selfie ENDs
